a4c413fb05253bf66c67ac659ffb57ae37027057
meaganmvs <meagansievers@gmail.com>
2018-02-24 16:23:16 -0500

    first

---
+++ app/controllers/search.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('giphySearchApp')
+  .controller('SearchCtrl', function ($scope, $http, $sce) {
+    $scope.search = {};
+    $scope.testing = 'elephant';
+    $scope.search.phrase = null;
+    $scope.apiKey = 'GZKGwdu6xlIM0iV58yFKJOFLqj0NLXFw';
+    var trustedUrl = $sce.trustAsResourceUrl('https://api.giphy.com/v1/gifs/search');
+    
+    $scope.searchGiphy = function () {
+        $http.get(trustedUrl+'?api_key='+$scope.apiKey+'&q='+$scope.search.phrase)
+            .then(function success (response) {
+                $scope.gifs = response.data.data;
+        }, function error (response) {
+            console.log(response);
+        });
+    };
+
+  });
