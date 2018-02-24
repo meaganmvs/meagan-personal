63ab38e5ae3c5a124b0559a623c8a28e403a72f3
meaganmvs <meagansievers@gmail.com>
2018-02-24 16:24:25 -0500

    test

--- app/app.js
+++ app/app.js
@@ -1,14 +1,32 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
-
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+/**
+ * GIPY Search App
+ * Created By Meagan Sievers
+ */
+angular
+  .module('giphySearchApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngMessages',
+    'ngRoute',
+    'ngMaterial'
+  ])
+  .config(function ($routeProvider) {
+    
+    $routeProvider
+      .when('/', {
+        templateUrl: 'index.html',
+        controller: 'SearchCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+    .directive('search', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'views/search.html',
+            controller: 'SearchCtrl'
+        };
+});
